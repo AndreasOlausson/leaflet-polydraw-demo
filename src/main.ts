124f422b1f9dae833df792bfab5f305c5ebd7d83
Andreas Olausson <andreas@codepoets.se>
2025-08-10 19:42:19 +0200

    new version 0.9.3

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 import Polydraw from 'leaflet-polydraw';
 import 'leaflet-polydraw/dist/leaflet-polydraw.css';
 
@@ -7,7 +8,7 @@ import 'leaflet-polydraw/dist/leaflet-polydraw.css';
 const map = L.map('map').setView([58.4108, 15.6214], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '<span>&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors</span>'
 }).addTo(map);
 
 const polydraw = new Polydraw();
